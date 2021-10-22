d90a321cdd187eda5cc87ae7ba194101a6dc0ca9
BartoszNiem <84670549+BartoszNiem@users.noreply.github.com>
2021-10-22 11:28:36 +0200

    Add files via upload

--- src/main/java/Entities/Travellers.java
+++ src/main/java/Entities/Travellers.java
@@ -1,4 +1,40 @@
 package Entities;
 
 public class Travellers {
+    private int numberOfRooms;
+    private int numberOfAdults;
+    private int numberOfChilds;
+
+    public Travellers() {
+    }
+
+    public Travellers(int numberOfRooms, int numberOfAdults, int numberOfChilds) {
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfAdults = numberOfAdults;
+        this.numberOfChilds = numberOfChilds;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public int getNumberOfAdults() {
+        return numberOfAdults;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+    }
+
+    public int getNumberOfChilds() {
+        return numberOfChilds;
+    }
+
+    public void setNumberOfChilds(int numberOfChilds) {
+        this.numberOfChilds = numberOfChilds;
+    }
 }
